b0e6e965db351be955e6bf6701adf9a040f6c97e
mrsvolodya <volodyamurskyi@gmail.com>
2025-03-07 12:57:07 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://drive.google.com/file/d/1MfB4GR5R-1eiZXMJ2gqUyF7N-9Wa1coz/view?usp=sharing
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="my-4 text-3xl font-bold flex">
+        <h1 className="text-blue-500 mx-auto">Inforce Header</h1>
+      </header>
+      <main className="flex-grow flex items-center justify-center bg-gray-900"></main>
+      <footer className="my-4 text-3xl font-bold flex">
+        <p className="text-blue-500 mx-auto">Footer</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
